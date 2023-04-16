4715cb21462cb3f4b084ec65f456b9978da4b9f9
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2023-04-16 12:01:24 +0800

    add announcement and information screen

--- components/AnnouncementScreen.jsx
+++ components/AnnouncementScreen.jsx
@@ -1,12 +1,81 @@
-import React from 'react'
-import { View, Text} from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, FlatList,TouchableOpacity,Linking } from 'react-native';
+import axios from 'axios';
 
 const AnnouncementScreen = () => {
-  return (
-    <View>
-        <Text>Announcement Screen</Text>
-    </View>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default AnnouncementScreen
+  const handleItemPress = (source) => {
+    if (source) {
+      Linking.openURL(source);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('https://ektm-backend.up.railway.app/api/announcement/all');
+      setData(response.data.data);
+    };
+    fetchData();
+  }, []);
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      <TouchableOpacity onPress={() => handleItemPress(item.source)}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.date}>{formatDate(item.schedule)}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Pengumuman</Text>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.title}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    paddingVertical: 3,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  date: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  content: {
+    fontSize: 16,
+    textAlign: 'center',
+  },
+});
+
+export default AnnouncementScreen;
--- components/InformationScreen.jsx
+++ components/InformationScreen.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
-import { View, Text} from 'react-native'
-const InformationScreen = () => {
-  return (
-    <View>
-        <Text>Information Screen</Text>
-    </View>
-  )
-}
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, Linking, FlatList } from 'react-native';
+import axios from 'axios';
 
-export default InformationScreen
+const InformationScreen = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('https://ektm-backend.up.railway.app/api/information/all');
+      setData(response.data.data);
+    };
+    fetchData();
+  }, []);
+
+  const handleItemPress = (source) => {
+    if (source) {
+      Linking.openURL(source);
+    }
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => handleItemPress(item.source)}>
+      <View style={styles.itemContainer}>
+        <Text style={styles.title}>{item.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Informasi</Text>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.title}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    paddingVertical: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default InformationScreen;
